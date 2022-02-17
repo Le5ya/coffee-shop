54097979e74b5d4ef1b02386e4e143bbb426f7eb
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2022-02-17 23:16:29 +0300

    lesson-2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
-  <div id="app">
-    Hello Vue.JS
+  <div class="wrapper">
+  <hero-view/>
+  <good-card-component/>
+  <footer-component/>
+  
   </div>
+  
 </template>
 
 <script>
+import HeroView from '@/views/HeroView.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
+import GoodCardComponent from '@/components/GoodCardComponent.vue'
+
 export default {
+  components: { HeroView, FooterComponent, GoodCardComponent }
  
 }
 </script>
