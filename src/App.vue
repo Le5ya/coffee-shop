2fbad7e7f2c56b3af5d4d3e3a45c5a10c8e793c2
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2022-02-21 21:38:23 +0300

    data,computed

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,103 @@
 <template>
-  <div class="wrapper">
+<div>
+  <h1>{{title}}</h1>
+   <ul>
+    <li>
+      <img :src="require(`@/assets/images/${posts[0].icon}`)" v-bind:alt="posts[0].icon">
+      <span>{{posts[0].text}}</span>
+    </li>
+    <li>
+      <img :src="require(`@/assets/images/${posts[1].icon}`)" v-bind:alt="posts[1].icon">
+      <span>{{posts[1].text}}</span>
+    </li>
+    <li>
+      <img :src="require(`@/assets/images/${posts[2].icon}`)" v-bind:alt="posts[2].icon">
+      <span>{{posts[2].text}}</span>
+    </li>
+    <li class="list-item" :class="posts[3].checked ? 'checked' : ''">
+      <img :src="require(`@/assets/images/${posts[3].icon}`)" v-bind:alt="posts[3].icon">
+      <span>{{posts[3].text}}</span>
+    </li>
+  </ul>
+  <span class="total">Всего строк: {{totalCount}}</span>
+</div>
+  <!-- <div class="wrapper">
   <router-view/>
 
   <footer-component/>
 
-  </div>
+  </div> -->
   
 </template>
 
 <script>
-import HeroView from '@/views/HeroView.vue'
-import ForYourPleasureView from '@/views/ForYourPleasureView.vue'
-import ContactUsView from '@/views/ContactUsView.vue'
-import FooterComponent from '@/components/FooterComponent.vue'
+// import HeroView from '@/views/HeroView.vue'
+// import ForYourPleasureView from '@/views/ForYourPleasureView.vue'
+// import ContactUsView from '@/views/ContactUsView.vue'
+// import FooterComponent from '@/components/FooterComponent.vue'
 
 
 export default {
-  components: { HeroView, ForYourPleasureView, ContactUsView, FooterComponent }
- 
-}
+  // components: { HeroView, ForYourPleasureView, ContactUsView, FooterComponent }
+  data() {
+    return {
+      title: 'Data, Computed',
+      posts: [
+        {
+          id: 0,
+          icon: 'setting.png',
+          text: 'текст первый',
+        },
+        {
+          id: 1,
+          icon: 'user.png',
+          text: 'текст второй',
+        },
+        {
+          id: 2,
+          icon: 'phone.png',
+          text: 'текст третий',
+        },
+        {
+          id: 3,
+          icon: 'phone.png',
+          text: 'текст сто третий',
+        },
+      ],
+    };
+  },
+  computed: {
+    totalCount(){
+      return this.posts.length + ' items'
+    }
+  }
+  };
 </script>
 
-<style>
+<style lang="scss">
+  ul {
+  list-style: none;
 
+  li {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+
+    img {
+      margin-right: 10px;
+    }
+
+    &:hover {
+      color: red;
+    }
+
+    &.checked {
+      color: red;
+    }
+  }
+}
+
+.total {
+  padding-left: 40px;
+}
 </style>
